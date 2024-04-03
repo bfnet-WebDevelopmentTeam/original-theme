c2d387b81f4360a946ed5b9a6cb8df1044df5e1e
bfnet-WebDevelopmentTeam <bfnet.0911@gmail.com>
2024-04-03 15:58:30 +0900

    commit

--- taxonomy.php
+++ taxonomy.php
@@ -9,19 +9,33 @@
     <div class="container works-container">
       <h2 class="section-title"><?php single_term_title(); ?>実績</h2>
       <?php
-      $works_cat = get_terms(array(
-        'taxonomy' => 'works_cat',
-        'include' => "web-production", "video-production", "sns",
-        'orderby' => 'slug',
-        'order' => 'DESC'
+      $tax_name = 'works_cat';
+      $term_slug = 'web-production';
+      $term_parent = get_terms($tax_name, array(
+        'slug' => $term_slug,
       ));
-      if (!empty($works_cat)) :
-        foreach ($works_cat as $cat) :
+      $term_child = get_terms($tax_name, array(
+        'parent' => $term_parent[0]->term_id,
+      ));
+
+      if (!empty($term_child)) :
+        foreach ($term_child as $term) :
       ?>
           <div class="container works-term-container">
-            <h3 class="works-term-heading"><?php echo $cat->name; ?></h3>
-           <?php get_template_part('template-parts/content-loop-works'); ?>
-            <a class="works-term-link" href="<?php echo get_term_link($cat); ?>"><?php echo $cat->name; ?>の一覧ページへ</a>
+            <h3 class="works-term-heading"><?php echo $term->name; ?></h3>
+            <?php
+            $args = array(
+              'post_type' => 'register_works',
+              'posts_per_page' => 4,
+            );
+            $the_query = new WP_Query($args);
+            if ($the_query->have_posts()) :
+              while ($the_query->have_posts()) : $the_query->the_post();
+                get_template_part('template-parts/content-loop-works');
+              endwhile;
+            endif;
+            ?>
+            <a class="works-term-link" href="<?php echo get_term_link($term); ?>"><?php echo $term->name; ?>の一覧ページへ</a>
           </div>
       <?php
         endforeach;
