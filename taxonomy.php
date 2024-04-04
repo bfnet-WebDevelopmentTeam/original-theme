f68b6574c310534a439687917a8a7597fcf261b7
bfnet-WebDevelopmentTeam <bfnet.0911@gmail.com>
2024-04-04 19:02:37 +0900

    commit

--- taxonomy.php
+++ taxonomy.php
@@ -9,38 +9,39 @@
     <div class="container works-container">
       <h2 class="section-title"><?php single_term_title(); ?>実績</h2>
       <?php
-      $tax_name = 'works_cat';
-      $term_slug = 'web-production';
-      $term_parent = get_terms($tax_name, array(
-        'slug' => $term_slug,
+      $parent_term = get_queried_object();
+      $child_terms = get_terms(array(
+        'taxonomy' => 'works_cat',
+        'parent' => $parent_term->term_id,
+        'hide_empty' => false,
       ));
-      $term_child = get_terms($tax_name, array(
-        'parent' => $term_parent[0]->term_id,
-      ));
-
-      if (!empty($term_child)) :
-        foreach ($term_child as $term) :
+      foreach ($child_terms as $term) :
       ?>
-          <div class="container works-term-container">
-            <h3 class="works-term-heading"><?php echo $term->name; ?></h3>
-            <?php
-            $args = array(
-              'post_type' => 'register_works',
-              'posts_per_page' => 4,
-            );
-            $the_query = new WP_Query($args);
-            if ($the_query->have_posts()) :
-              while ($the_query->have_posts()) : $the_query->the_post();
-                get_template_part('template-parts/content-loop-works');
-              endwhile;
-            endif;
-            ?>
-            <a class="works-term-link" href="<?php echo get_term_link($term); ?>"><?php echo $term->name; ?>の一覧ページへ</a>
-          </div>
-      <?php
-        endforeach;
-      endif;
-      ?>
+        <div class="container works-term-container">
+          <h3 class="works-term-heading"><?php echo $term->name; ?></h3>
+          <?php
+          $args = array(
+            'post_type' => 'register_works',
+            'tax_query' => array(
+              array(
+                'taxonomy' => 'works_cat',
+                'field' => 'term_id',
+                'terms' => $term->term_id,
+              ),
+            ),
+            'posts_per_page' => 4,
+          );
+          $query = new WP_Query($args);
+          if ($query->have_posts()) :
+            while ($query->have_posts()) : $query->the_post();
+              get_template_part('template-parts/content-loop-works');
+            endwhile;
+          endif;
+          wp_reset_postdata();
+          ?>
+          <a class="works-term-link" href="<?php echo get_term_link($term); ?>"><?php echo $term->name; ?>の一覧ページへ</a>
+        </div>
+      <?php endforeach; ?>
     </div>
   </div>
 </main>
