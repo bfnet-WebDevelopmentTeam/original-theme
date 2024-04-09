e07fa3543512fd4a6436a716ddfa51ed42f6a398
bfnet-WebDevelopmentTeam <bfnet.0911@gmail.com>
2024-04-09 11:03:57 +0900

    commit

--- taxonomy.php
+++ taxonomy.php
@@ -8,148 +8,28 @@
   <div class="inner">
     <div class="container works-container">
       <h2 class="section-title"><?php single_term_title(); ?>実績</h2>
-      <?php
-     
-      ?>
-        <div class="container works-term-container">
-        <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_93913736_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_61303825_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/web01.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_74833875_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_91941047_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_84366447_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_74833875_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_74833875_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_78357634_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_42940200_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_73138703_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-            <div class="works-term-box">
-              <a href="single-works.html">
-                <div class="box-img">
-                  <img src="imgs/pixta_74833875_M.jpg" alt="" />
-                </div>
-                <p class="works-term-txt">
-                  テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。
-                </p>
-              </a>
-            </div>
-        </div>
-      <?php 
-      
-      ?>
+      <div class="container works-term-container">
+        <?php
+        if (have_posts()) :
+          while (have_posts()) : the_post();
+            get_template_part('template-parts/content-loop-works');
+          endwhile;
+        endif;
+        ?>
+      </div>
     </div>
     <div class="wp-pagenavi" role="navigation">
-          <span aria-current="page" class="current">1</span>
-          <a class="page larger" title="ページ 2" href="#">2</a>
-          <a class="page larger" title="ページ 3" href="#">3</a>
-          <a class="page larger" title="ページ 4" href="#">4</a>
-          <a class="page larger" title="ページ 5" href="#">5</a>
-          <span class="extend">...</span>
-          <a class="larger page" title="ページ 10" href="#">10</a>
-          <span class="extend">...</span>
-          <a class="nextpostslink" rel="next" aria-label="次のページ" href="#">»</a>
-          <a class="last" aria-label="Last Page" href="#">最後 »</a>
-        </div>
+      <span aria-current="page" class="current">1</span>
+      <a class="page larger" title="ページ 2" href="#">2</a>
+      <a class="page larger" title="ページ 3" href="#">3</a>
+      <a class="page larger" title="ページ 4" href="#">4</a>
+      <a class="page larger" title="ページ 5" href="#">5</a>
+      <span class="extend">...</span>
+      <a class="larger page" title="ページ 10" href="#">10</a>
+      <span class="extend">...</span>
+      <a class="nextpostslink" rel="next" aria-label="次のページ" href="#">»</a>
+      <a class="last" aria-label="Last Page" href="#">最後 »</a>
+    </div>
   </div>
 </main>
 <?php get_footer(); ?>
